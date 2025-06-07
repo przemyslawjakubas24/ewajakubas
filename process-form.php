789a01d7d5249a74b36d2bfbdcd213f4171522ac
przemyslawjakubas24 <przemyslawjakubas24@gmail.com>
2025-06-07 19:04:35 +0200

    Uruchomienie formularza, dodanie biblioteki katalogu PHPMailer-master na serwer, poprawne ustawienie serwera

--- process-form.php
+++ process-form.php
@@ -1,9 +1,11 @@
 <?php
-// Importuj bibliotekę PHPMailer
+// Importuj klasy PHPMailer do globalnej przestrzeni nazw
+// Te deklaracje muszą znajdować się na początku skryptu, nie wewnątrz funkcji
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-// Ręczne dołączanie potrzebnych plików
+// Ścieżka do biblioteki PHPMailer (zmień ścieżkę jeśli jest inna)
 require 'PHPMailer-master/src/Exception.php';
 require 'PHPMailer-master/src/PHPMailer.php';
 require 'PHPMailer-master/src/SMTP.php';
@@ -24,27 +26,29 @@ $email_content .= "Rodzaj wydarzenia: $event\n";
 $email_content .= "Data wydarzenia: $date\n\n";
 $email_content .= "Wiadomość:\n$message\n";
 
-// Utworzenie nowego obiektu PHPMailer
+// Utwórz instancję; przekazanie `true` włącza wyjątki
 $mail = new PHPMailer(true);
 
 try {
     // Ustawienia serwera
-    $mail->isSMTP();                                      // Użyj SMTP
-    $mail->Host       = 'mail.testystrony.pl';           // Serwer SMTP
-    $mail->SMTPAuth   = true;                             // Włącz uwierzytelnianie SMTP
-    $mail->Username   = 'kontakt@testystrony.pl';           // Nazwa użytkownika SMTP
-    $mail->Password   = 'nkrbdF4aDgD78uve3bMg';                    // Hasło SMTP
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;   // Włącz szyfrowanie TLS
-    $mail->Port       = 465;                              // Port TCP do połączenia
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Wyłącz szczegółowe informacje debugowania (ustaw DEBUG_SERVER dla debugowania)
+    $mail->isSMTP();                                         // Wyślij używając SMTP
+    $mail->Host       = 'h58.seohost.pl';                  // Ustaw serwer SMTP do wysyłania (zmień na swój)
+    $mail->SMTPAuth   = true;                                // Włącz uwierzytelnianie SMTP\\
+    $mail->Username   = 'form@testystrony.pl';                  // Nazwa użytkownika SMTP (zmień na swoją)
+    $mail->Password   = 'Tmobile1';                            // Hasło SMTP (zmień na swoje) CZY TO HASŁO JEST PRAWIDŁOWE?
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;         // Włącz domyślne szyfrowanie TLS
+    $mail->Port       = 465;                                 // Port TCP do połączenia; użyj 587 jeśli ustawiłeś `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail->CharSet    = 'UTF-8';                             // Ustaw kodowanie znaków na UTF-8 dla polskich znaków
 
     // Odbiorcy
-    $mail->setFrom('kontakt@testystrony.pl', 'Formularz kontaktowy');
-    $mail->addAddress('jakubas.tbg@gmail.com');            // Dodaj odbiorcę
-    $mail->addReplyTo($email, $name);                     // Odpowiedź na adres nadawcy
+    $mail->setFrom('jakubas.tbg@gmail.com', 'Formularz kontaktowy');  // Od kogo (adres formularza)
+    $mail->addAddress('form@testystrony.pl');                       // Dodaj odbiorcę (zmień na swój)
+    $mail->addReplyTo($email, $name);                                 // Adres zwrotny (adres osoby wypełniającej)
 
     // Treść
-    $mail->isHTML(false);                                 // Format emaila jako zwykły tekst
-    $mail->Subject = "Nowa wiadomość ze strony ewajakubas.pl";
+    $mail->isHTML(false);                                    // Format wiadomości jako tekst (false = tekst, true = HTML)
+    $mail->Subject = 'Nowa wiadomość ze strony testystrony.pl';
     $mail->Body    = $email_content;
 
     $mail->send();
