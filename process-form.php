dbcc6f9b8f3a4bfe2a460d8bf141656257aa106e
przemyslawjakubas24 <przemyslawjakubas24@gmail.com>
2025-06-08 17:40:08 +0200

    Zmiany 1.08.06.2025

--- process-form.php
+++ process-form.php
@@ -19,7 +19,7 @@ $date = $_POST['date'] ?? '';
 $message = $_POST['message'] ?? '';
 
 // Treść wiadomości
-$email_content = "Imię i nazwisko: $name\n";
+$email_content = "Imię: $name\n";
 $email_content .= "Email: $email\n";
 $email_content .= "Telefon: $phone\n";
 $email_content .= "Rodzaj wydarzenia: $event\n";
@@ -31,19 +31,19 @@ $mail = new PHPMailer(true);
 
 try {
     // Ustawienia serwera
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Wyłącz szczegółowe informacje debugowania (ustaw DEBUG_SERVER dla debugowania)
+    $mail->SMTPDebug = SMTP::DEBUG_OFF;                      // Wyłącz szczegółowe informacje debugowania (ustaw DEBUG_SERVER dla debugowania)
     $mail->isSMTP();                                         // Wyślij używając SMTP
     $mail->Host       = 'h58.seohost.pl';                  // Ustaw serwer SMTP do wysyłania (zmień na swój)
     $mail->SMTPAuth   = true;                                // Włącz uwierzytelnianie SMTP\\
     $mail->Username   = 'form@testystrony.pl';                  // Nazwa użytkownika SMTP (zmień na swoją)
-    $mail->Password   = 'Tmobile1';                            // Hasło SMTP (zmień na swoje) CZY TO HASŁO JEST PRAWIDŁOWE?
+    $mail->Password   = 'Tmobile1';                            // Hasło SMTP (zmień na swoje)
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;         // Włącz domyślne szyfrowanie TLS
     $mail->Port       = 465;                                 // Port TCP do połączenia; użyj 587 jeśli ustawiłeś `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
     $mail->CharSet    = 'UTF-8';                             // Ustaw kodowanie znaków na UTF-8 dla polskich znaków
 
     // Odbiorcy
-    $mail->setFrom('jakubas.tbg@gmail.com', 'Formularz kontaktowy');  // Od kogo (adres formularza)
-    $mail->addAddress('form@testystrony.pl');                       // Dodaj odbiorcę (zmień na swój)
+    $mail->setFrom('form@testystrony.pl', 'Formularz kontaktowy');  // Od kogo (adres formularza)
+    $mail->addAddress('kontakt@testystrony.pl');                       // Dodaj odbiorcę (zmień na swój)
     $mail->addReplyTo($email, $name);                                 // Adres zwrotny (adres osoby wypełniającej)
 
     // Treść
